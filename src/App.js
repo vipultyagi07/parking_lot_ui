ef6bea4aebd5aef970dc03082235f8840198310a
Vipul Tyagi <vipultyagiabes@gmail.com>
2024-06-29 20:35:41 +0530

    ADDED:: new Send otp page with simple api fetch hit method

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Registration from "./component/Registration";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './component/Home'
 import Login from './component/Login';
+import SendOtp from './component/SendOtp';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/" element={<SendOtp/>} />
         <Route path="/registration" element={<Registration />} />
       </Routes>
     </BrowserRouter>
