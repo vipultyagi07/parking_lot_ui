6daf9ddc83ba701c3f8b930a746fd2847ba3e642
Vipul Tyagi <vipultyagiabes@gmail.com>
2024-06-30 14:49:35 +0530

    dummy1

--- src/component/BookTicket.js
+++ src/component/BookTicket.js
@@ -1,78 +1,123 @@
-import React from 'react'
-import Footer from './Footer'
-import Content from './Content'
-import Header from './Header'
-import { ReactComponent as Motorcycle } from './twowheeler.svg';
-import { ReactComponent as Fourwheeler } from './fourwheeler.svg';
-import { ReactComponent as Ticket } from './ticket.svg';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Footer from "./Footer";
+import Header from "./Header";
+import { ReactComponent as Motorcycle } from "./twowheeler.svg";
+import { ReactComponent as Fourwheeler } from "./fourwheeler.svg";
+import { ReactComponent as Ticket } from "./ticket.svg";
 
 function BookTicket() {
+  const [twoWheelerData, setTwoWheelerData] = useState({
+    totalSpot: 0,
+    freeSpot: 0,
+  });
+  const [fourWheelerData, setFourWheelerData] = useState({
+    totalSpot: 0,
+    freeSpot: 0,
+  });
+
+  useEffect(() => {
+    const fetchTwoWheelerData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8081/api/parking-spot/free/spot/TWO_WHEELER"
+        );
+        setTwoWheelerData(response.data);
+      } catch (error) {
+        console.error("Error fetching two wheeler data:", error);
+      }
+    };
+
+    const fetchFourWheelerData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8081/api/parking-spot/free/spot/FOUR_WHEELER"
+        );
+        setFourWheelerData(response.data);
+      } catch (error) {
+        console.error("Error fetching four wheeler data:", error);
+      }
+    };
+
+    fetchTwoWheelerData();
+    fetchFourWheelerData();
+  }, []);
+
   return (
     <div>
-      <Header/>
+      <Header />
       <div>
-      <div>
-            <section id="booking">
-                <h2>Booking</h2>
-                <div className="booking-container">
-                    <div className="pricing-card">
-                        <a href="/">
-                            <Motorcycle className="social-icon" />
-                        </a>                
-                        <h3>2 Wheeler</h3>
-                        <p>Total Spots: 50</p>
-                        <p>Free Spots: 20</p>
-                        <p>Filled Spots: 30</p>
-                    </div>
+        <div>
+          <section id="booking">
+            <h2>Booking</h2>
+            <div className="booking-container">
+              <div className="pricing-card">
+                <a href="/">
+                  <Motorcycle className="social-icon" />
+                </a>
+                <h3>2 Wheeler</h3>
+                <p>Total Spots: {twoWheelerData.totalSpot}</p>
+                <p>Free Spots: {twoWheelerData.freeSpot}</p>
+                <p>
+                  Filled Spots:{" "}
+                  {twoWheelerData.totalSpot - twoWheelerData.freeSpot}
+                </p>
+              </div>
 
-                    <div className="card large-card">
-                        <a href="/">
-                            <Ticket className="social-icon" />
-                        </a>
-                        <h3>Generate Ticket</h3>
-                        <form id="ticket-form">
-                            <label htmlFor="vehicle-type">Select Vehicle Type:</label>
-                            <select id="vehicle-type" name="vehicle-type">
-                                <option value="2wheeler">2 Wheeler</option>
-                                <option value="4wheeler">4 Wheeler</option>
-                            </select>
-                            <button type="submit" className="cta">Proceed</button>
-                        </form>
-                    </div>
-                    <div className="pricing-card">
-                        <a href="/">
-                            <Fourwheeler className="social-icon" />
-                        </a>
-                        <h3>4 Wheeler</h3>
-                        <p>Total Spots: 30</p>
-                        <p>Free Spots: 10</p>
-                        <p>Filled Spots: 20</p>
-                    </div>
-                </div>
-            </section>
-            <section id="ticket-info">
-                <h2>Ticket Information</h2>
-                <div className="ticket-info-container">
-                    <div className="card">
-                        <h3>Live Ticket See</h3>
-                        <p>Total Live Tickets: 100</p>
-                        <br />
-                        <button className="cta">View</button>
-                    </div>
-                    <div className="card">
-                        <h3>Total Generated Tickets</h3>
-                        <p>Total Tickets: 100</p>
-                        <button className="cta">View</button>
-                    </div>
-                    <div className="card">
-                        <h3>Payment Info</h3>
-                        <p>Total Revenue: $5000</p>
-                        <p>Last Payment: $50</p>
-                        <button className="cta">View</button>
-                    </div>
-                </div>
-            </section>
-            <section id="pricing">
+              <div className="card large-card">
+                <a href="/">
+                  <Ticket className="social-icon" />
+                </a>
+                <h3>Generate Ticket</h3>
+                <form id="ticket-form">
+                  <label htmlFor="vehicle-type">Select Vehicle Type:</label>
+                  <select id="vehicle-type" name="vehicle-type">
+                    <option value="2wheeler">2 Wheeler</option>
+                    <option value="4wheeler">4 Wheeler</option>
+                  </select>
+                  <button type="submit" className="cta">
+                    Proceed
+                  </button>
+                </form>
+              </div>
+
+              <div className="pricing-card">
+                <a href="/">
+                  <Fourwheeler className="social-icon" />
+                </a>
+                <h3>4 Wheeler</h3>
+                <p>Total Spots: {fourWheelerData.totalSpot}</p>
+                <p>Free Spots: {fourWheelerData.freeSpot}</p>
+                <p>
+                  Filled Spots:{" "}
+                  {fourWheelerData.totalSpot - fourWheelerData.freeSpot}
+                </p>
+              </div>
+            </div>
+          </section>
+          <section id="ticket-info">
+            <h2>Ticket Information</h2>
+            <div className="ticket-info-container">
+              <div className="card">
+                <h3>Live Ticket See</h3>
+                <p>Total Live Tickets: 100</p>
+                <br />
+                <button className="cta">View</button>
+              </div>
+              <div className="card">
+                <h3>Total Generated Tickets</h3>
+                <p>Total Tickets: 100</p>
+                <button className="cta">View</button>
+              </div>
+              <div className="card">
+                <h3>Payment Info</h3>
+                <p>Total Revenue: $5000</p>
+                <p>Last Payment: $50</p>
+                <button className="cta">View</button>
+              </div>
+            </div>
+          </section>
+          <section id="pricing">
             <h2>Pricing</h2>
             <div className="pricing-container">
               <div className="pricing-card">
@@ -122,9 +167,9 @@ function BookTicket() {
           </section>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default BookTicket
+export default BookTicket;
